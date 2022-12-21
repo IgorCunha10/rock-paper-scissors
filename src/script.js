0c320649fbfda2a94467a3810f5262f2e3ddcd50
IgorCunha10 <gigor497@gmail.com>
2022-12-21 11:57:10 -0300

    created first functions

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+const rock = 'Rock';
+const paper = 'Paper';
+const scissor = 'Scissor'
+}
+ 
+function playRound (playerSelection, computerSelection) {
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
